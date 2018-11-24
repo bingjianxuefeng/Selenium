36b6e65c179f707d7f7f550e4577c6c066bfbce3
bingjianxuefeng <425145298@qq.com>
2018-11-24 18:17:06 +0800

    *

---
+++ Selenium/runall2.py
@@ -0,0 +1,12 @@
+# encoding: utf-8
+"""
+@author:Administrator
+@file: runall2.py
+@time: 2018/11/24
+"""
+
+import pytest
+
+if __name__ == "__main__":
+    args = ['-s', '-q']
+    pytest.main(args)
