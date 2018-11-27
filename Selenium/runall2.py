a88ea7efefccf4be5402c5034f17390a350d61a5
bingjianxuefeng <425145298@qq.com>
2018-11-27 15:55:57 +0800

    ''

--- Selenium/runall2.py
+++ Selenium/runall2.py
@@ -7,7 +7,7 @@
 
 import pytest
 import sys
-
+import os
 if __name__ == "__main__":
     # 启动测试,通过接收cmd参数启动.被用在客户机上被远程调用的时候使用.
     #sys.argv用来接收shell或者是windows批处理传递的参数
@@ -15,7 +15,10 @@ if __name__ == "__main__":
     s_len = sys.argv.__len__()
     s_argv = sys.argv
     s_args = ['-s', '-q']
-    args = s_args + s_argv[1:]
+    s_arg = s_argv[1:]
+    xml_file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'Report'))
+    args = s_args + s_arg[:1]
+    args.append(xml_file_path)
     # print(s_len)
     # print(s_argv)
     # print(s_argv[1:])
