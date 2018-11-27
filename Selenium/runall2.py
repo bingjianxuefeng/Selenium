2ebde135471ddcf0f2ff06e9601a655dd4c86db3
bingjianxuefeng <425145298@qq.com>
2018-11-27 11:17:02 +0800

    '接收shell或者是window批处理传入的参数'

--- Selenium/runall2.py
+++ Selenium/runall2.py
@@ -6,7 +6,18 @@
 """
 
 import pytest
+import sys
 
 if __name__ == "__main__":
-    args = ['-s', '-q']
+    # 启动测试,通过接收cmd参数启动.被用在客户机上被远程调用的时候使用.
+    #sys.argv用来接收shell或者是windows批处理传递的参数
+
+    s_len = sys.argv.__len__()
+    s_argv = sys.argv
+    s_args = ['-s', '-q']
+    args = s_args + s_argv[1:]
+    # print(s_len)
+    # print(s_argv)
+    # print(s_argv[1:])
+    # print(args)
     pytest.main(args)
