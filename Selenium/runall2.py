d2230a63eabdb3802fef2f0e733c27ca362bea64
bingjianxuefeng <425145298@qq.com>
2018-11-27 17:50:21 +0800

    ''

--- Selenium/runall2.py
+++ Selenium/runall2.py
@@ -16,9 +16,7 @@ if __name__ == "__main__":
     s_argv = sys.argv
     s_args = ['-s', '-q']
     s_arg = s_argv[1:]
-    xml_file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'Report'))
-    args = s_args + s_arg[:1]
-    args.append(xml_file_path)
+    args = s_args + s_arg[1:]
     # print(s_len)
     # print(s_argv)
     # print(s_argv[1:])
